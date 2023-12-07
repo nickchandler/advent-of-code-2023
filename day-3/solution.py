69a34bce2ff59e73ca506d3f82c6feed682c59e0
Nicholas Chandler <nicholaschandler@Nicholass-MacBook-Air.local>
2023-12-06 19:28:01 -0800

    wrong solution low

--- day-3/solution.py
+++ day-3/solution.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, 'input.txt')
@@ -23,11 +24,14 @@ def isPart(char):
         return True
 
 # find a number if it exists, and blank it out
-def find_num(line, x):
+def find_num(y, x, input, seen_dict):
+    if y < 0 or y > input.__len__() - 1: return False
+    line = input[y]
     if x < 0 or x > line.__len__() - 1: return False
     if not safe_int(line[x]): return False
     num_string = [line[x]]
-    print(num_string, 'this is num_string')
+    seen_dict[y][x] = True
+
     # go left
     x_left = 1
     while True:
@@ -36,12 +40,12 @@ def find_num(line, x):
 
         if safe_int(line[x - x_left]):
             num_string.insert(0, line[x - x_left])
+            seen_dict[y][x - x_left] = True
 
         else: break
 
         x_left += 1
 
-    print(num_string, 'this is num_string after going left')
     # go right
     x_right = 1
     while True:
@@ -50,17 +54,20 @@ def find_num(line, x):
 
         if safe_int(line[x + x_right]):
             num_string = num_string + [line[x + x_right]]
+            seen_dict[y][x + x_right] = True
 
         else: break
 
         x_right += 1
 
-    print(num_string, 'this is num_string after going right')
-
-    return [safe_int("".join(num_string))]
+    return safe_int("".join(num_string))
         
 def solution(input):
     sum = 0
+    ## Pass the seen dict into find_num so that we can "blank the number out by recording where we've seen numbers.
+    ## You'll need to now pass line number into find_num to accurately report the coordinates of where we've seen numbers before.
+    seen_dict = defaultdict(lambda: defaultdict(lambda: None))
+
 
     matrix_len = input.__len__()
     
@@ -76,70 +83,57 @@ def solution(input):
                 print('LOOKING FOR STUFF')
                 # look for numbers and blank them out
                 # up
-                if line_num - 1 > 0:
-                    line = input[line_num - 1]
-                    if not line == None:
-                        maybe_num = find_num(line, x)
+                if line_num - 1 > 0 and not seen_dict[line_num -1][x]: 
+                        maybe_num = find_num(line_num - 1, x, input, seen_dict)
                         if maybe_num:
                             sum += maybe_num
                         
                     # up left
-                if line_num - 1 > 0:
-                    line = input[line_num - 1]
-                    if not line == None:
+                if line_num - 1 > 0 and not seen_dict[line_num - 1][x - 1]:
                         if x > 0:
-                            maybe_num = find_num(line, x - 1)
+                            maybe_num = find_num(line_num - 1, x - 1, input, seen_dict)
                             if maybe_num:
                                 sum += maybe_num
                         
                     # up right
-                if line_num - 1 > 0:
-                    line = input[line_num - 1]
-                    if not line == None:
-                        maybe_num = find_num(line, x + 1)
+                if line_num - 1 > 0 and not seen_dict[line_num - 1][x + 1]:
+                        maybe_num = find_num(line_num - 1, x + 1, input, seen_dict)
                         if maybe_num:
                             sum += maybe_num
                         
                     # right 
-                if line_num - 1 > 0:
-                    line = input[line_num]
-                    if not line == None:
-                        maybe_num = find_num(line, x + 1)
+                if  not seen_dict[line_num][x + 1]:
+                        maybe_num = find_num(line_num, x + 1, input, seen_dict)
                         if maybe_num:
                             sum += maybe_num
                         
                     # down right
-                if line_num + 1 < matrix_len - 1:
-                    line = input[line_num + 1]
-                    if not line == None:
-                        maybe_num = find_num(line, x + 1)
+                if line_num + 1 < matrix_len - 1 and not seen_dict[line_num + 1][x + 1]:
+                        maybe_num = find_num(line_num + 1, x + 1, input, seen_dict)
                         if maybe_num:
                             sum += maybe_num
 
                     # down 
-                if line_num + 1 < matrix_len - 1:
-                    line = input[line_num + 1]
-                    if not line == None:
-                        maybe_num = find_num(line, x)
+                if line_num + 1 < matrix_len - 1 and not seen_dict[line_num + 1][x]:
+                        maybe_num = find_num(line_num + 1, x, input, seen_dict)
                         if maybe_num:
                             sum += maybe_num
 
                     # down left
-                if line_num + 1 < matrix_len - 1:
-                    line = input[line_num + 1]
-                    if not line == None:
+                if line_num + 1 < matrix_len - 1 and not seen_dict[line_num + 1][x -1]:
                         if x > 0:
-                            maybe_num = find_num(line, x - 1)
+                            maybe_num = find_num(line_num + 1, x - 1, input, seen_dict)
                             if maybe_num:
                                 sum += maybe_num
 
                     # left
-                    line = input[line_num]
-                    if not line == None:
-                        if x > 0:
-                            maybe_num = find_num(line, x - 1)
+                        if x > 0 and not seen_dict[line_num][x - 1]:
+                            maybe_num = find_num(line_num, x - 1, input, seen_dict)
                             if maybe_num:
+                                print(maybe_num, 'this is maybe_num')
+                                print(sum, 'this is sum')
                                 sum += maybe_num
+    print(input)
 
     return sum
 
